3e08712fe9f2da81786f7e07f1de467787dc6d33
tobiaswai <jacky115980726@gmail.com>
2024-01-27 16:49:37 +0800

    create a Django project in Template, navbar & products page

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("products", views.products, name="products"),
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     context={}
     return render(request, "myApp/home.html", context)
+
+def products(request):
+    context={}
+    return render(request, "myApp/products.html", context)
