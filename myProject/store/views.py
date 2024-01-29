4f27d4018a04677a8a4a6cc4016d8efc45be9223
tobiaswai <jacky115980726@gmail.com>
2024-01-29 23:22:54 +0800

    Created Order/OrderItem (not finished)

--- myProject/store/views.py
+++ myProject/store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 
 def store(request):
@@ -31,3 +33,30 @@ def checkout(request):
 
      context = {'items':items, 'order':order}
      return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+     data = json.loads(request.data)
+     productID = data['productID']
+     action = data['action']
+
+     print('Action:', action)
+     print('productID:', productID)
+
+     customer = request.user.customer
+     product = product.objects.get(id=productID)
+     order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+     OrderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+     if action == 'add':
+          OrderItem.quantity = (OrderItem.quantity + 1)
+     elif action == 'remove':
+          OrderItem.quantity = (OrderItem.quantity - 1)
+
+     OrderItem.save()
+
+     if OrderItem.quantity <= 0:
+          OrderItem.delete()
+
+     return JsonResponse('Item was added', safe=False)
