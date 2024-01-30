f1e4cb721109da389d3a895ff6a5fa9844312606
tobiaswai <jacky115980726@gmail.com>
2024-01-30 23:50:37 +0800

    Added to Cart Functionality

--- myProject/store/models.py
+++ myProject/store/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=True)
+    digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -29,12 +29,21 @@ class Product(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=100, null=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
     
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+
     @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
--- myProject/store/views.py
+++ myProject/store/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import *
 
 def store(request):
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
+     else:
+          items = []
+          order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}    
+          cartItems = order['get_cart_items'] 
+
      products = Product.objects.all()
-     context = {'products':products}
+     context = {'products':products, 'cartItems':cartItems}
      return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -14,11 +26,13 @@ def cart(request):
           customer = request.user.customer
           order, created = Order.objects.get_or_create(customer=customer, complete=False)
           items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
      else:
           items = []
-          order = {'get_cart_total':0, 'get_cart_items':0}
+          order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+          cartItems = order['get_cart_items']
 
-     context = {'items':items, 'order':order}
+     context = {'items':items, 'order':order, 'cartItems':cartItems}
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -26,12 +40,14 @@ def checkout(request):
            customer = request.user.customer
            order, created = Order.objects.get_or_create(customer=customer, complete=False)
            items = order.orderitem_set.all()
+           cartItems = order.get_cart_items
      else:
           items = []
-          order = {'get_cart_total':0, 'get_cart_items':0}
+          order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+          cartItems = order['get_cart_items']
 
 
-     context = {'items':items, 'order':order}
+     context = {'items':items, 'order':order, 'cartItems':cartItems}
      return render(request, 'store/checkout.html', context)
 
 
@@ -60,3 +76,31 @@ def updateItem(request):
           orderItem.delete()
 
      return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+     transaction_id = datetime.datetime.now().timestamp()
+     data = json.loads(request.body)
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          total = float(data['form']['total'])
+          order.transaction_id = transaction_id
+
+          if total == order.get_cart_total:
+               order.complete = True
+          order.save()
+
+          if order.shipping == True:
+               ShippingAddress.objects.create(
+                    customer=customer,
+                    order=order,
+                    address=data['shipping']['address'],
+                    city=data['shipping']['city'],
+                    state=data['shipping']['state'],
+                    zipcode=data['shipping']['zipcode'],
+                    )
+     
+     else:
+          print('User is not logged in...')
+     return JsonResponse('Payment complete!', safe=False)
