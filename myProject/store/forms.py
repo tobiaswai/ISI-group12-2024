3059d2373e9459f479bfacbfe147cb5eadcc2591
tobiaswai <jacky115980726@gmail.com>
2024-03-09 18:52:36 +0800

    updated models.py and pages

--- myProject/store/forms.py
+++ myProject/store/forms.py
@@ -4,12 +4,5 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name','price','size','weight','image','description','is_active']
-    def clean_image(self):
-        image = self.cleaned_data.get('image')
-        return image
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['name','price','size','weight','image','image2','image3','description','is_active']
+        fields = ['name','price','brand','color','image','image2','image3','description','is_active']
+    
