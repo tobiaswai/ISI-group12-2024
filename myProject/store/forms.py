3b881e65e8106d3aea58b429b336220cadb914bc
Tobias Wai <jacky115980726@gmail.com>
2024-03-25 14:53:12 +0800

    updated multi photos

--- myProject/store/forms.py
+++ myProject/store/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Product
+from .models import *
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name','price','brand','connectivity_technology','cover_image','description','is_active']
+    
+    class Image:
+        model = Image
+        fields = ['image']
     
